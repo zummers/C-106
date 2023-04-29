84576e88b135683c52aa523215a6f1ca196826ab
zummers <83545395+zummers@users.noreply.github.com>
2023-04-29 12:20:40 +0100

    Add files via upload

---
+++ PRO-C106-Student-Boilerplate-main/face_detect.py
@@ -0,0 +1,19 @@
+import cv2
+
+img = cv2.imread("4f.jpg")
+
+gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+faces = face_cascade.detectMultiScale(gray)
+print(len(faces))
+
+for (x,y,w,h) in faces:
+       cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+       roi_color=img[y:y+h,x:x+w]
+       cv2.imwrite("face.jpg",roi_color)
+             
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
